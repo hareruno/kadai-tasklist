874ee9ec0e60ec9d008c1239592f1f39a661d6a3
ryosuke.k <rk.programming04@gmail.com>
2020-06-08 04:47:58 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  root to: "tasks#index"
+  resources :tasks
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
-
-=begin
-  root to: "tasks#index"
-  resources :tasks
-=end
+  
+  #resources :tasklists, only: [:create, :destroy]
 end
